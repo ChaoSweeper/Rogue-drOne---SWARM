aea2945264d43181989d0af1282664088691a7dd
James Cowell <ChaoSweeper@users.noreply.github.com>
2020-10-22 16:46:20 -0400

    Add folder for code and cv
    Added the required folders the code this project will generate.

---
+++ Code/CV/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="Rogue drOne SWARM",
+    version="0.0.1",
+    author="James Cowell",
+    author_email="jcowell1@uncc.edu",
+    license="Apache-2.0",
+    packages=[""],
+)
